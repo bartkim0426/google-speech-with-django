c7d941976cab2c6c989e431cf140c6b6b7736ef2
bartkim0426 <bartkim0426@gmail.com>
2017-09-25 12:25:08 +0900

    EDIT : update production settings
    Files:
    *
    
    Why:
    *
    
    ETC:
    *

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,13 +6,13 @@ from .partials import *
 DEBUG = False
 INTERNAL_IPS = ['1.234.21.73', ]
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': '',
-#         'USER': os.environ["POSTGRES_USER"],
-#         'PASSWORD': os.environ["POSTGRES_PASSWORD"],
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ["POSTGRES_DB_NAME"],
+        'USER': os.environ["POSTGRES_USER"],
+        'PASSWORD': os.environ["POSTGRES_PASSWORD"],
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
